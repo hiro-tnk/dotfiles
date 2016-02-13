bf84dca6a10c76046910d240b9a7f24e19b08886
hiro-tnk <parakeet0101@yahoo.co.jp>
2016-02-13 22:12:41 +0900

    change to make .dot file if file link already exists

--- install.sh
+++ install.sh
@@ -4,8 +4,15 @@ do
     [[ "$f" == ".DS_Store" ]] && continue
     [[ "$f" == ".git_ignore" ]] && continue
 
-    if [ ! -e ~/$f ]; then
-        echo linking: $f
-        ln -s ~/dotfiles/$f ~/$f
+    if [ -e $HOME/$f ]; then
+      if [ -f $f ]; then
+        echo making .dot: $f
+        ln -s $HOME/dotfiles/$f ~/$f.dot
+      else
+        echo the directory link already exists: $f
+      fi
+    else
+      echo linking: $f
+      ln -s $HOME/dotfiles/$f ~/$f
     fi
 done
